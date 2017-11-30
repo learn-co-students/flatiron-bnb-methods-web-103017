0c06ae2e7245aef0f9c97d56d921b39d5cc130e0
pawlkris <paulkristapovich@gmail.com>
2017-11-30 16:49:42 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,57 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+ def city_openings(datein, dateout)
+    #takes a date range
+    #finds listing within that range
+    #outputs those listing
+    openings = []
+    self.listings.each do |listings|
+      listings.reservations.each do |reservation|
+        checkin = reservation.checkin
+        checkout = reservation.checkout
+        #binding.pry
+        if [datein.to_date, checkin].max > [dateout.to_date, checkout].min
+          openings << listings
+        end
+      end
 
+     openings
+
+   end
+  end
+
+ def self.highest_ratio_res_to_listings
+    ratio = {}
+
+   self.all.each do |city|
+      listings = city.listings.count
+      reservations = 0
+      city.listings.each do |listing|
+        reservations += listing.reservations.count
+      end
+      ratio[city] = (reservations.to_f/listings.to_f)
+
+   end
+
+   ratio.max_by{|k,v| v}[0]
+
+ end
+
+ def self.most_res
+    ratio = {}
+
+   self.all.each do |city|
+      reservations = 0
+      city.listings.each do |listing|
+        reservations += listing.reservations.count
+      end
+      ratio[city] = (reservations)
+
+   end
+
+   ratio.max_by{|k,v| v}[0]
+
+ end
+
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,28 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :title, :address, :listing_type, :description, :price, :neighborhood_id,  presence: true
+
+  after_save :set_host_as_host
+  before_destroy :unset_host_as_host
+
+  def unset_host_as_host
+   # remove .id
+   if Listing.where(host: host).where.not(id: id).empty?
+     host.update(host: false)
+   end
+ end
+
+ def set_host_as_host
+     host.update(host: true)
+ end
+
+ def average_review_rating
+   self.reviews.collect{|x|x.rating}.inject(0){|sum,x| sum + x }.to_f/self.reviews.count.to_f
+ end
+
+
+
+
 end
