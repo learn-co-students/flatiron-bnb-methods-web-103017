2fe16cd50be0a6d1785b079269be6bddc892bdc2
RochelLevi <rochlevi@gmail.com>
2017-11-30 14:29:58 -0500

    working o available listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,12 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  def available?(checkin, checkout)
+    self.reservations.find do |reservation|
+      reservation.checkin < checkout && reservation.checkout > checkin
+    end
+  end
+
+
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,13 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(checkin, checkout)
+    # checkin_parsed = checkin.split('-')
+    # checkout_parsed = checkout.split('-')
+    checkin_date = Date.parse(checkin)
+    checkout_date = Date.parse(checkout)
+    self.listings.select do |listing|
+      listing.available?(checkin_date, checkout_date)
+    end
+  end
 end
