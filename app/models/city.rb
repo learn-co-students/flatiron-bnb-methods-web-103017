c765b6fe5385330a2d1d078527603c2543cc9d7a
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-11-30 15:37:02 -0500

    Trying to solve city method

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,30 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(d1, d2)
+    self.listings.select do |listing|
+      # needs a select method
+    #   listing.reservations.collect do |reservation|
+    #     range = (DateTime.parse(d1)...DateTime.parse(d2))
+    #     !range.include?(reservation.checkin) && !range.include?(reservation.checkout)
+    #   end
+    # end
+  end
 end
 
+  #will return a boolean
+  def date_range_check(d1, d2)
+    range = (DateTime.parse(d1)...DateTime.parse(d2))
+    !range.include?(reservation.checkin) && !range.include?(reservation.checkout)
+  end
+
+  #needs to return only if the collection is all 'true' booleans
+  def reservations_check(d1, d2)
+    self.reservations.collect do |reservation|
+      if reservation.date_range_check(d1, d2) ==
+    end
+  end
+
+  def opening_check
+
+  end
