d53676f9f70e1aed0740ddca330aa7fcfe2c147e
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-11-30 17:32:02 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,29 +3,55 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
 
   def city_openings(d1, d2)
-    self.listings.select do |listing|
-      # needs a select method
-    #   listing.reservations.collect do |reservation|
-    #     range = (DateTime.parse(d1)...DateTime.parse(d2))
-    #     !range.include?(reservation.checkin) && !range.include?(reservation.checkout)
-    #   end
-    # end
+    result = []
+    self.listings.each do |listing|
+      array = []
+      listing.reservations.each do |reservation|
+        range = (DateTime.parse(d1)...DateTime.parse(d2))
+        # is reservation outside range
+        if !range.include?(reservation.checkin) && !range.include?(reservation.checkout)
+          array << reservation
+        end
+      end
+      result << listing if array.length == listing.reservations.length
+     end
+    result
   end
-end
 
-  #will return a boolean
-  def date_range_check(d1, d2)
-    range = (DateTime.parse(d1)...DateTime.parse(d2))
-    !range.include?(reservation.checkin) && !range.include?(reservation.checkout)
-  end
-
-  #needs to return only if the collection is all 'true' booleans
-  def reservations_check(d1, d2)
-    self.reservations.collect do |reservation|
-      if reservation.date_range_check(d1, d2) ==
+  def self.highest_ratio_res_to_listings
+    ratio = 0
+    city_value = nil
+    self.all.each do |city|
+      ratio_inst = city.total_reservations/city.total_listings
+      if ratio_inst > ratio
+        ratio = ratio_inst
+        city_value = city
+      end
     end
+    city_value
   end
 
-  def opening_check
-
+  def self.most_res
+    result = 0
+    city_value = nil
+    self.all.each do |city|
+      if city.total_reservations > result
+        result = city.total_reservations.to_i
+        city_value = city
+      end
+    end
+    city_value
   end
+  #amount of the reservation per listing
+  def total_reservations
+    self.listings.collect do |listing|
+      listing.reservations.length
+    end.first.to_f
+  end
+
+  #total number of listings per city
+  def total_listings
+    self.listings.length.to_f
+  end
+
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,22 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood_id, presence: true
+
+  before_save :host_setter
+
+
+
+  def host_setter
+    self.host.host = true
+    self.host.save
+  end
+
+
 end
