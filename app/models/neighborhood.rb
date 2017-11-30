e2f572e7507e69a33451bd9feddcd9533ae08e91
github username <github email address>
2017-11-30 17:43:12 -0500

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,59 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+
+  def neighborhood_openings(start_date, end_date)
+
+    date_range = (start_date.to_date..end_date.to_date)
+    open_listings = self.listings.select do |listing|
+      # if at any point a reservation is found then goodbye listing
+      reservations_check = listing.reservations.reject do |res|
+        #exclude any scenario where our reservation is unavailable
+        date_range === (res.checkin) || date_range === (res.checkout)
+      end
+      listing.reservations.length == reservations_check.length
+    end #end listings do
+    open_listings
+  end # end city_openings
+
+  def self.highest_ratio_res_to_listings
+    hash = {}
+    Neighborhood.all.each do |hood|
+
+
+      reservations_count = 0
+      listings = hood.listings.count
+      reservations = hood.listings.each do |listing|
+        reservations_count += listing.reservations.count
+      end
+
+      if listings > 0
+        hash[hood] = {reservations: reservations_count , listings: listings, ratio: reservations_count.to_f/listings.to_f}
+      else
+        hash[hood] = {reservations: 1 , listings: 1, ratio: 0}
+      end
+      # reservations = city.listings.each{|listing| listing.reservations}
+    end
+    hash
+    # binding.pry
+    winner = hash.sort_by{|k,v| v[:ratio]}.last.flatten.first
+
+  end
+
+  def self.most_res
+    hash = {}
+    Neighborhood.all.each do |hood|
+
+
+      reservations_count = 0
+      listings = hood.listings.count
+      reservations = hood.listings.each do |listing|
+
+        reservations_count += listing.reservations.count
+      end
+      hash[hood] = {reservations: reservations_count , listings: listings}
+    end
+    hash.sort_by{|k,v| v[:reservations]}.last.flatten.first
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,87 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :check_checkin
+  validate :check_checkout
+  validate :validate_host_is_not_guest
+  validate :validate_available_at_checkin
+  validate :validate_available_at_checkout
+  validate :checkin_before_checkout
+  validate :checkin_and_checkout_not_equal
+
+
+
+
+  def check_checkin
+    if !self.checkin.is_a?(Date)
+      errors.add(:checkin, "Invalid date")
+    end
+  end
+
+  def check_checkout
+    if !self.checkout.is_a?(Date)
+      errors.add(:checkout, "Invalide date")
+    end
+  end
+
+
+  def validate_host_is_not_guest
+    host = self.listing.host_id
+    if self.guest_id == host
+      errors.add(:guest_id, "Guest cannot be the host.")
+    end
+  end
+
+
+  def validate_available_at_checkin
+    self.listing.reservations.each do |reservation|
+      if (reservation.checkin..reservation.checkout).include?(self.checkin)
+        errors.add(:checkin, "Reservation time unavailable")
+      end
+    end
+  end
+
+  def validate_available_at_checkout
+    self.listing.reservations.each do |reservation|
+      if (reservation.checkin..reservation.checkout).include?(self.checkout)
+        errors.add(:checkout, "Reservation time unavailable")
+      end
+    end
+  end
+
+
+  def checkin_before_checkout
+    if !self.checkin.nil? && !self.checkout.nil?
+      if self.checkout < self.checkin
+        errors.add(:checkin, "Checkout must be after Checkin")
+      end
+    end
+  end
+
+  def checkin_and_checkout_not_equal
+    if self.checkin == self.checkout
+      errors.add(:checkin, "Checkin and Checkout cannot be the same")
+      errors.add(:checkout, "Checkin and Checkout cannot be the same")
+    end
+  end
+
+  def checkin_is_date
+    if !self.checkin.is_a?(Date)
+      errors.add(:checkin, "Checkin invalid")
+    end
+  end
+
+  def duration
+    self.checkout - self.checkin
+  end
+
+  def total_price
+    self.duration * self.listing.price
+
+  end
+
+
+
 end
